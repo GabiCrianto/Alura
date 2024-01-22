f03cadb3eda0bd809500a6e052b92d90aa99dc4c
GabiCrianto <157414164+GabiCrianto@users.noreply.github.com>
2024-01-22 16:17:23 -0300

    Add files via upload

---
+++ pessoas.js
@@ -0,0 +1,18 @@
+let palavraPessoa = (quantidadePessoas);
+
+let quantidadePessoas = quantidadePessoas  == 1 ? 'pessoa' : 'pessoas';
+
+
+
+
+
+
+
+
+
+
+if (quantidadePessoas == 1){
+    palavraPessoa = "pessoa";
+}else{
+    palavraPessoa = "pessoas"
+}
